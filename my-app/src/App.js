cb10ae1c7da417f97497163a12bb3608f392bbee
Prateek Agrawal <prateek.bka@gmail.com>
2023-02-23 12:20:33 +0530

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,1 @@
 
-import { ChakraProvider } from '@chakra-ui/react';
-import HomePage from './HomePage/HomePage';
-import { Navbar } from './HomePage/Navbar';
-
-function App() {
-  return (
-    <div className="App">
-      <ChakraProvider>
-        <Navbar/>
-        <HomePage/>
-      </ChakraProvider>
-        
-      
-    </div>
-  );
-}
-
-export default App;

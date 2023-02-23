2518103ba100f41165427ffbc1d71fde0ac4d82b
Sameerpadhy50 <sameerpadhy50@gmail.com>
2023-02-24 03:17:10 +0530

    day-2 added

--- my-app/src/HomePage/HomeData.js
+++ my-app/src/HomePage/HomeData.js
@@ -48,26 +48,173 @@ export const data = [
     {
       id: 1,
       src: "https://ii3.pepperfry.com/media/wysiwyg/banners/Home_Web_21022023_6.jpg",
-      head:"FOr ₹ 549 at 66% off ",
-      title: " 2 x 7ft Floor Runner",
+      head:"For ₹ 549 at 66% off ",
+      title: " 2 x 7 ft Floor Runner",
     },
     {
       id: 2,
       src: "https://ii1.pepperfry.com/media/wysiwyg/banners/Home_Web_21022023_13.jpg",
-      head:"FOr ₹ 159 at 54% off ",
+      head:"For ₹ 159 at 54% off ",
       title: " Set of 4 Side Plates",
     },
     {
       id: 3,
       src: "https://ii3.pepperfry.com/media/wysiwyg/banners/Home_Web_21022023_11.jpg",
-      head:" FOr ₹ 129 at 90% off ",
+      head:" For ₹ 129 at 90% off ",
       title: "3 Meter LED String Light",
     },
     {
       id: 4,
       src: "https://ii2.pepperfry.com/media/wysiwyg/banners/Hp_section4_web_04012023_4.jpg",
-      head:" FOr ₹ 129 at 71% off ",
+      head:" For ₹ 129 at 71% off ",
       title: " Broken Heart Natural Plant",
     },
   ];
+
+  // ------------Inspire and Get inspire-----------------
+
+  export const inspire_get = [
+    {
+      id: 1,
+      src: "https://ii3.pepperfry.com/media/wysiwyg/banners/Home_Web_21022023_6.jpg",
+      head:"For ₹ 549 at 66% off ",
+      title: " 2 x 7 ft Floor Runner",
+    },
+    {
+      id: 2,
+      src: "https://ii1.pepperfry.com/media/wysiwyg/banners/Home_Web_21022023_13.jpg",
+      head:"For ₹ 159 at 54% off ",
+      title: " Set of 4 Side Plates",
+    },
+    {
+      id: 3,
+      src: "https://ii3.pepperfry.com/media/wysiwyg/banners/Home_Web_21022023_11.jpg",
+      head:" For ₹ 129 at 90% off ",
+      title: "3 Meter LED String Light",
+    },
+    {
+      id: 4,
+      src: "https://ii2.pepperfry.com/media/wysiwyg/banners/Hp_section4_web_04012023_4.jpg",
+      head:" For ₹ 129 at 71% off ",
+      title: " Broken Heart Natural Plant",
+    },
+  ];
+
+  //--------------------------Furniture and Decor----
+
+  export const furniture_decor=[{
+    id: 1,
+    src: "https://ii1.pepperfry.com/media/wysiwyg/banners/Hp_section6_web_02022023_1.jpg",
+    head:"Cozy Luxe Wing Chairs",
+    price:13999 ,
+  },{
+    id: 2,
+    src: "https://ii2.pepperfry.com/media/wysiwyg/banners/Hp_section6_web_02022023_2.jpg",
+    head:"Tactile Material Lights",
+    price:700,
+  },{
+    id: 3,
+    src: "https://ii3.pepperfry.com/media/wysiwyg/banners/Hp_section6_web_02022023_3.jpg",
+    head:"Eclectric Bar Furniture",
+    price:3299,
+  },]
+  //----------------------customer speaks
+  export const customer_speaks=[{
+    id: 1,
+    src: "https://ii1.pepperfry.com/media/wysiwyg/banners/Testimonial_web_261221_7.jpg",
+    head:"Bought these two grey puffy stools from pepperfry. Found them to be surprisingly strong! Blends perfectly with our",
+    name:"Suman Senapatia" ,
+    place:"Visakhapatnam"
+  },{
+    id: 2,
+    src: "https://ii3.pepperfry.com/media/wysiwyg/banners/happy_customer_block_2_2906_0222.png",
+    head:"I purchased Candy Study Chair in Pink Colour by Alex Daisy from pepperfry looks awesome and My daughter loves and",
+    name:"Satya Mandhana",
+    place:"Secunderabad"
+  },{
+    id: 3,
+    src: "https://ii2.pepperfry.com/media/wysiwyg/banners/Testimonial_web_261221_9.jpg",
+    head:"Purchased an ergonomic chair, I thought it would not be fitting for me but once it got delivered, got it assembled",
+    name:"Baba Kattubadi",
+    place:"Hyderabad"
+  },]
+  // -------------------------------shopCategories
+   export const shopCategories=[
+    {id:1,
+      url: "https://ii2.pepperfry.com/media/wysiwyg/banners/Decor_Web_23022023_1.jpg",
+      title:"Spider Natural Plant"
+    },
+    {id:2,
+      url: "https://ii3.pepperfry.com/media/wysiwyg/banners/Decor_Web_23022023_2.jpg",
+      title:"Macrame Wall Hanging"
+    },
+    {id:3,
+      url: "https://ii3.pepperfry.com/media/wysiwyg/banners/decor_web_section3_14102022_2.jpg",
+      title:"Decorative Mirror"
+    },
+    {id:4,
+      url: "https://ii1.pepperfry.com/media/wysiwyg/banners/decor_web_section3_14102022_3.jpg",
+      title:"Home Fragnance"
+    },
+    {id:5,
+      url: "https://ii2.pepperfry.com/media/wysiwyg/banners/decor_web_section3_14102022_4.jpg",
+      title:"Candle Holders"
+    },
+    {id:6,
+      url: "https://ii3.pepperfry.com/media/wysiwyg/banners/Decor_section3_web_06122022_5.jpg",
+      title:"Diyas"
+    },
+    {id:7,
+      url: "https://ii1.pepperfry.com/media/wysiwyg/banners/decor_web_section3_14102022_6.jpg",
+      title:"Pooja Mandirs"
+    },
+    {id:8,
+      url: "https://ii1.pepperfry.com/media/wysiwyg/banners/Decor_trend_web_05122022_2.jpg",
+      title:"Urlis, Pooja Thali"
+    }
+  ]
+  // -----------------------Recommended for You
+  export const recommend=[
+    {id:1,
+      url: "https://ii1.pepperfry.com/media/catalog/product/r/o/494x544/round-glass-tube-15-minutes-sky-blue-sand-timer-showpiece-by-exim-decor-round-glass-tube-15-minutes--aoe9sw.jpg",
+      title:"Round Glass TUbe",
+      price:699
+    },
+    {id:2,
+      url: "https://ii1.pepperfry.com/media/catalog/product/r/o/494x544/round-glass-tube-10-minutes-pink-sand-timer-showpiece-by-exim-decor-round-glass-tube-10-minutes-pink-mcs8zp.jpg",
+      title:"Round Gladd Tube",
+      price:649
+    },
+    {id:3,
+      url: "https://ii1.pepperfry.com/media/catalog/product/r/o/494x544/round-glass-tube-3-minutes-orange-sand-timer-showpiece-by-exim-decor-round-glass-tube-3-minutes-oran-nckqlt.jpg",
+      title:"Decorative Mirror",
+      price:599
+    },
+    {id:4,
+      url: "https://ii1.pepperfry.com/media/catalog/product/r/a/494x544/raw-matt-gold--aluminium-handcrafted-nautical-sailing-boat--by-decor-de-maison-raw-matt-gold--alumin-7ofool.jpg",
+      title:"Home Fragnance",
+      price:359
+    },
+    {id:5,
+      url: "https://ii1.pepperfry.com/media/catalog/product/s/o/494x544/solidarity-black-aluminium-globe-stand-by-decor-de-maison-solidarity-black-aluminium-globe-stand-by--qoujhi.jpg",
+      title:"Candle Holders",
+      price:2359
+    },
+    {id:6,
+      url: "https://ii2.pepperfry.com/media/wysiwyg/banners/hp_need_help_p_5.jpg",
+      title:"Diyas",
+      price:2399
+    },
+    {id:7,
+      url: "https://ii1.pepperfry.com/media/wysiwyg/banners/hp_need_help_p_4.jpg",
+      title:"Pooja Mandirs",
+      price:5999
+    },
+    {id:8,
+      url: "https://ii1.pepperfry.com/media/wysiwyg/banners/Decor_trend_web_05122022_2.jpg",
+      title:"Urlis, Pooja Thali",
+      price:2549
+    }
+  ]
+
   
